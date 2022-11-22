3a4e2c355d55f0326ec7fb55db128fdad054a646
rafael rubenstein <73216681+rafaelrubenstein@users.noreply.github.com>
2022-11-22 18:23:59 -0500

    Started working on streamlit app

---
+++ Stonks Streamlit app/home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd 
+
+
+st.set_page_config(
+    page_title="Stonks LLC",
+    page_icon="💸",
+)
+
+# Create a page header
+st.header("Welcome To The Future Of Investing")
